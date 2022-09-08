d0af2d3a4096de2a5f074188d2aa9e7e6e96dc02
ryuliantoro <rio@integria.co.id>
2022-09-08 19:18:35 +0200

    hooks

--- tuamuda_cms/hooks.py
+++ tuamuda_cms/hooks.py
@@ -41,7 +41,7 @@ app_logo_url = '/assets/logo.png'
 # ----------
 
 # application home page (will override Website Settings)
-# home_page = "login"
+home_page = "login"
 
 # website user home page (by Role)
 # role_home_page = {
