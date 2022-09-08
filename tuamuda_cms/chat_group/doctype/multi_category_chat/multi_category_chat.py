20954a45a64e4000a66d44a1aca4e5f4b4ae388b
ryuliantoro <rio@integria.co.id>
2022-09-08 18:03:35 +0200

    doctypes dan workspaces

---
+++ tuamuda_cms/chat_group/doctype/multi_category_chat/multi_category_chat.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Dev INP and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class MultiCategoryChat(Document):
+	pass
---
+++ tuamuda_cms/video_rooms/doctype/room/room.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Dev INP and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Room', {
+	// refresh: function(frm) {
+
+	// }
+});
