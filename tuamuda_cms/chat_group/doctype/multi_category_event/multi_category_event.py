4b0ff023eef7474e8138cfd0c6c55a80bbf652b2
ryuliantoro <rio@integria.co.id>
2022-09-13 13:37:15 +0200

    multi select table

--- tuamuda_cms/chat_group/doctype/multi_category_event/multi_category_event.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2022, Dev INP and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class MultiCategoryEvent(Document):
-	pass
