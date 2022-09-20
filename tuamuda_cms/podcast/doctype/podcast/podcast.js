76c31d426e4bb1cf83fb35bd9d428cc479e6a74d
ryuliantoro <rio@integria.co.id>
2022-09-20 09:55:00 +0200

    podcast detail page

--- tuamuda_cms/podcast/doctype/podcast/podcast.js
+++ tuamuda_cms/podcast/doctype/podcast/podcast.js
@@ -5,4 +5,14 @@ frappe.ui.form.on('Podcast', {
 	// refresh: function(frm) {
 
 	// }
+    video_source: function(frm){
+        if ( frm.doc.video_source == "Server" ) {
+            frappe.msgprint("Saat ini Server belum bisa dipilih sebagai sumber video")
+        }
+    },
+    validate: function(frm){
+        if ( frm.doc.video_source == "Server" ) {
+            frappe.throw("Saat ini Server belum bisa dipilih sebagai sumber video.<br> Harap ganti sumber video sebelum Save.")
+        }
+    }
 });
