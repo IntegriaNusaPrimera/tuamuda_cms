878b9c0232a3b5a5b4cd50cdafd4930f18a8d841
ryuliantoro <rio@integria.co.id>
2022-10-02 18:07:59 +0200

    public files

--- tuamuda_cms/public/js/web_script.js
+++ tuamuda_cms/public/js/web_script.js
@@ -11,3 +11,16 @@ function barlogo() {
 
 document.addEventListener( "DOMContentLoaded", barlogo );
 
+
+function catChevron() {
+    let w1 = document.getElementById( 'cat-chooser' ).offsetWidth
+    let w2 = document.getElementById( 'cat-chooser' ).getElementsByTagName( 'li' ).length * 100
+    if ( w1 < w2 + 45 ) {
+        let left = "<i class='fa fa-chevron-left text-muted float-left my-2 pl-1 pr-2'></i>";
+        document.getElementById('chevron-left').innerHTML = left;
+        let right = "<i class='fa fa-chevron-right text-muted float-right my-2 pl-2 pr-1'></i>";
+        document.getElementById('chevron-right').innerHTML = right;
+    }
+}
+
+document.addEventListener( "DOMContentLoaded", catChevron );
